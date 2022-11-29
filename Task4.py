ac9f53d9f8989a174d0016011bc659a4f968137c
Kairat <burambaevkairat@mail.ru>
2022-11-29 21:03:35 +0300

    curly braces removed=)

--- Task4.py
+++ Task4.py
@@ -2,12 +2,12 @@ part = int(input('Введите номер четверти: '))
 q = list(range(11))
 c = list(range(-10,1))
 if (part == 1):
-    {print('x:',q,'y:',q)}
+    print('x:',q,'y:',q)
 elif (part == 2):
-    {print('x:',c,'y:',q)}
+    print('x:',c,'y:',q)
 elif (part == 3):
-    {print('x:',c,'y:',c)}
+    print('x:',c,'y:',c)
 elif (part == 4):
-    {print('x:',q,'y:',c)}
+    print('x:',q,'y:',c)
 else:
-    {print('Введите число от 1 до 4!')}
+    print('Введите число от 1 до 4!')
